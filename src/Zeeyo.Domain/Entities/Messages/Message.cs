3d6f936e85cde505e39fb2dd5620687adeb970fc
Mukhammadzokir <139150511+MuxammadZokir@users.noreply.github.com>
2024-12-18 11:11:13 +0500

    Created Message class

--- src/Zeeyo.Domain/Entities/Messages/Message.cs
+++ src/Zeeyo.Domain/Entities/Messages/Message.cs
@@ -1,12 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Zeeyo.Domain.Commons;
 
-namespace Zeeyo.Domain.Entities.Messages
+namespace Zeeyo.Domain.Entities.Messages;
+
+public class Message : Auditable
 {
-    internal class Message
-    {
-    }
+    public string Subject { get; set; }
+    public string Body { get; set; }
+    public string To { get; set; }
 }

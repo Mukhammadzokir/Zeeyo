43d8c56c1161f239066246170aac5a708ab8de05
Mukhammadzokir <139150511+MuxammadZokir@users.noreply.github.com>
2024-12-18 11:06:46 +0500

    Created Branch

---
+++ src/Zeeyo.Domain/Entities/Branches/Branch.cs
@@ -0,0 +1,16 @@
+﻿using Zeeyo.Domain.Commons;
+using Zeeyo.Domain.Entities.Users;
+
+namespace Zeeyo.Domain.Entities.Branches;
+
+public class Branch : Auditable
+{
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public string PhoneNumber { get; set; }
+    public string Address { get; set; }
+    public string Longitude { get; set; }
+    public string Latitude { get; set; }
+    public ICollection<User> Users { get; set; }
+    public ICollection<Course> Courses { get; set; }
+}

037e2678ce6c0c964808a47ec10c976adb90b5f1
Mukhammadzokir <139150511+MuxammadZokir@users.noreply.github.com>
2024-12-18 11:03:39 +0500

    Created Classes belongs to Users Table

---
+++ src/Zeeyo.Domain/Entities/Users/UserRole.cs
@@ -0,0 +1,11 @@
+﻿using Zeeyo.Domain.Commons;
+
+namespace Zeeyo.Domain.Entities.Users;
+
+public class UserRole : Auditable
+{
+    public long UserId { get; set; }
+    public User User { get; set; }
+    public long RoleId { get; set; }
+    public Role Role { get; set; }
+}

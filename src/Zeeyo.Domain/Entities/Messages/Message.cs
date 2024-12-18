146b42be3151917efe95803a55976448ffb7b319
Mukhammadzokir <139150511+MuxammadZokir@users.noreply.github.com>
2024-12-18 11:10:45 +0500

    Created Message class

---
+++ src/Zeeyo.Domain/Entities/Messages/Message.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zeeyo.Domain.Entities.Messages
+{
+    internal class Message
+    {
+    }
+}

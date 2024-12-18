313094e7a6fa85ec80b295abd137a1c8a7857008
Mukhammadzokir <139150511+MuxammadZokir@users.noreply.github.com>
2024-12-18 11:09:01 +0500

    Craeted Course and Lesson classes

---
+++ src/Zeeyo.Domain/Entities/Courses/Course.cs
@@ -0,0 +1,20 @@
+﻿using Zeeyo.Domain.Commons;
+using Zeeyo.Domain.Entities.Branches;
+
+namespace Zeeyo.Domain.Entities.Courses;
+
+public class Course : Auditable
+{
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public decimal Price { get; set; }
+    public TimeOnly StartTime { get; set; }
+    public TimeOnly EndTime { get; set; }
+    public long BranchId { get; set; }
+    public Branch Branch { get; set; }
+    public DateTime EndDate { get; set; }
+    public ICollection<Lesson> Lessons { get; set; }
+    public ICollection<Enrollment> Students { get; set; }
+    public ICollection<Attendance> Attendances { get; set; }
+    public ICollection<TeacherCourse> Teachers { get; set; }
+}
---
+++ src/Zeeyo.Domain/Entities/Courses/Lesson.cs
@@ -0,0 +1,13 @@
+﻿using Zeeyo.Domain.Commons;
+
+namespace Zeeyo.Domain.Entities.Courses;
+
+public class Lesson : Auditable
+{
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public string Content { get; set; }
+    public long CourseId { get; set; }
+    public Course Course { get; set; }
+    public DateTime Date { get; set; }
+}

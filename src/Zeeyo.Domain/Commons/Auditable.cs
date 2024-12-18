c6b6ea568f5536e47ba8a2a2d83cb97310618e4e
Mukhammadzokir <139150511+MuxammadZokir@users.noreply.github.com>
2024-12-18 10:56:08 +0500

    Created Auditable and Asset classes

---
+++ src/Zeeyo.Domain/Commons/Auditable.cs
@@ -0,0 +1,11 @@
+﻿namespace Zeeyo.Domain.Commons
+{
+    public abstract class Auditable
+    {
+        public long Id { get; set; }
+        public bool IsDeleted { get; set; } = false;
+        public DateTime? UpdatedAt { get; set; }
+        public DateTime? DeletedAt { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    }
+}
---
+++ src/Zeeyo.Domain/Entities/Assets/Asset.cs
@@ -0,0 +1,13 @@
+﻿using Zeeyo.Domain.Commons;
+
+namespace Zeeyo.Domain.Entities.Assets
+{
+    public abstract class Asset : Auditable
+    {
+        public string Name { get; set; }
+        public string Path { get; set; }
+        public string Extension { get; set; }
+        public long Size { get; set; }
+        public string Type { get; set; }
+    }
+}

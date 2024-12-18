cfe0040b105a6ec99c9e2c8fb6d2b55c271b6463
Mukhammadzokir <139150511+MuxammadZokir@users.noreply.github.com>
2024-12-18 11:13:34 +0500

    Created Payment class

---
+++ src/Zeeyo.Domain/Entities/Payments/Payment.cs
@@ -0,0 +1,14 @@
+﻿using Zeeyo.Domain.Commons;
+using Zeeyo.Domain.Entities.Branches;
+
+namespace Zeeyo.Domain.Entities.Payments;
+
+public class Payment : Auditable
+{
+    public long StudentId { get; set; }
+    public Student Student { get; set; }
+    public long BranchId { get; set; }
+    public Branch Branch { get; set; }
+    public decimal Amount { get; set; }
+    public DateTime Date { get; set; }
+}

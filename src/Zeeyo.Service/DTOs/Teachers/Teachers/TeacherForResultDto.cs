65b8bd7a8f2e54400117e382c185201f06db3567
Mukhammadzokir <139150511+MuxammadZokir@users.noreply.github.com>
2024-12-21 00:06:46 +0500

    Created Teacher and TeacherCourse DTOs

---
+++ src/Zeeyo.Service/DTOs/Teachers/Teachers/TeacherForResultDto.cs
@@ -0,0 +1,22 @@
+﻿using Zeeyo.Domain.Entities.Branches;
+using Zeeyo.Domain.Entities.Users;
+using Zeeyo.Service.DTOs.Teachers.TeacherCourses;
+
+namespace Zeeyo.Service.DTOs.Teachers.Teachers;
+
+public class TeacherForResultDto
+{
+    public long Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Specialization { get; set; }
+    public string PhoneNumber { get; set; }
+    public string TelegramUserName { get; set; }
+    public DateTime DateOfBirth { get; set; }
+    public long UserId { get; set; }
+    public User User { get; set; }
+    public long BranchId { get; set; }
+    public Branch Branch { get; set; }
+    public TeacherProfilePhotoForResultDto ProfilePhoto { get; set; }
+    public ICollection<TeacherCourseForResultDto> Courses { get; set; }
+}
---
+++ src/Zeeyo.Service/DTOs/Teachers/Teachers/TeacherForUpdateDto.cs
@@ -0,0 +1,13 @@
+﻿namespace Zeeyo.Service.DTOs.Teachers.Teachers;
+
+public class TeacherForUpdateDto
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Specialization { get; set; }
+    public string PhoneNumber { get; set; }
+    public string TelegramUserName { get; set; }
+    public DateTime DateOfBirth { get; set; }
+    public long UserId { get; set; }
+    public long BranchId { get; set; }
+}

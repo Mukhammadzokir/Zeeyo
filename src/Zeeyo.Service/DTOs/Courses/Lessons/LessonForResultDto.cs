c2f9374d473674018e8102a53235bcc6a14f23f2
Mukhammadzokir <139150511+MuxammadZokir@users.noreply.github.com>
2024-12-20 23:38:37 +0500

    Created Branche, Lesson and Course Dtos

---
+++ src/Zeeyo.Service/DTOs/Courses/Lessons/LessonForResultDto.cs
@@ -0,0 +1,14 @@
+﻿using Zeeyo.Domain.Entities.Courses;
+
+namespace Zeeyo.Service.DTOs.Courses.Lessons;
+
+public class LessonForResultDto
+{
+    public long Id { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public string Content { get; set; }
+    public long CourseId { get; set; }
+    public Course Course { get; set; }
+    public DateTime Date { get; set; }
+}

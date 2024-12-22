65f413881ab848a9e06e241fdde144194cef2512
Mukhammadzokir <139150511+MuxammadZokir@users.noreply.github.com>
2024-12-22 23:53:51 +0500

    Done ServiceExtension

---
+++ src/Zeeyo.Api/Extensions/ServiceExtension.cs
@@ -0,0 +1,22 @@
+﻿using Zeeyo.Service.Mappers;
+using Zeeyo.Data.Repositories;
+using Zeeyo.Data.IRepositories;
+using Zeeyo.Service.Services.Branches;
+using Zeeyo.Service.Interfaces.Branches;
+
+namespace Zeeyo.Api.Extensions;
+
+public static class ServiceExtension
+{
+    public static void AddCustomService(this IServiceCollection services)
+    {
+        // Mapper
+        services.AddAutoMapper(typeof(MappingProfile));
+        
+        // Repository
+        services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
+
+        // Services
+        services.AddScoped<IBranchService, BranchService>();
+    }
+}

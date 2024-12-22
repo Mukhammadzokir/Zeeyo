0b2695fb84e8329b8f21b8d11ee35a881850ba4d
Mukhammadzokir <139150511+MuxammadZokir@users.noreply.github.com>
2024-12-22 22:57:17 +0500

    Created IBranchService

---
+++ src/Zeeyo.Service/Interfaces/Branches/IBranchService.cs
@@ -0,0 +1,14 @@
+﻿using Zeeyo.Service.DTOs.Branches;
+using Zeeyo.Service.Configurations;
+
+namespace Zeeyo.Service.Interfaces.Branches;
+
+public interface IBranchService
+{
+    Task<bool> RemoveAsync(long id);
+    Task<BranchForResultDto> RetrieveByIdAsync(long id);
+    Task<BranchForResultDto> AddAsync(BranchForCreationDto dto);
+    Task<BranchForResultDto> ModifyAsync(long id, BranchForUpdateDto dto);
+    Task<IEnumerable<BranchForResultDto>> RetrieveAllAsync(PaginationParams @params);
+    Task<IEnumerable<BranchForResultDto>> SearchAllAsync(string search, PaginationParams @params);
+}

c3d998d100875d62976af8a42d01350ad9ac5ebb
Mukhammadzokir <139150511+MuxammadZokir@users.noreply.github.com>
2024-12-23 13:21:52 +0500

    Modified Auditable

--- src/Zeeyo.Domain/Commons/Auditable.cs
+++ src/Zeeyo.Domain/Commons/Auditable.cs
@@ -4,9 +4,9 @@
     {
         public long Id { get; set; }
         public bool IsDeleted { get; set; } = false;
-        public long DeletedBy { get; set; }
+        public long? DeletedBy { get; set; }
         public DateTime? UpdatedAt { get; set; }
-        public long UpdatedBy { get; set; }
+        public long? UpdatedBy { get; set; }
         public DateTime? DeletedAt { get; set; }
         public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
     }

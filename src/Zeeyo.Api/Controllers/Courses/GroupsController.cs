cbe42eeffb9205b4f45592212d299b99ac98e8b7
Mukhammadzokir <zokr.net@gmail.com>
2025-01-17 15:56:22 +0500

    Done GroupsController

---
+++ src/Zeeyo.Api/Controllers/Courses/GroupsController.cs
@@ -0,0 +1,103 @@
+﻿using Zeeyo.Api.Models;
+using Microsoft.AspNetCore.Mvc;
+using Zeeyo.Service.Configurations;
+using Zeeyo.Api.Controllers.Commons;
+using Zeeyo.Service.Interfaces.Courses;
+using Zeeyo.Service.DTOs.Courses.Groups;
+
+namespace Zeeyo.Api.Controllers.Courses;
+
+public class GroupsController : BaseController
+{
+    private readonly IGroupService _groupService;
+    public GroupsController(IGroupService groupService)
+    {
+        _groupService = groupService;
+    }
+
+    /// <summary>
+    /// To Create group
+    /// </summary>
+    /// <param name="dto"></param>
+    /// <returns></returns>
+    [HttpPost]
+    public async Task<IActionResult> PostAsync([FromBody] GroupForCreationDto dto)
+        => Ok(new Response
+        {
+            StatusCode = 200,
+            Message = "Success",
+            Data = await _groupService.AddAsync(dto)
+        });
+
+    /// <summary>
+    /// To Get all groups
+    /// </summary>
+    /// <param name="params"></param>
+    /// <returns></returns>
+    [HttpGet]
+    public async Task<IActionResult> GetAllAsync([FromQuery] PaginationParams @params)
+        => Ok(new Response
+        {
+            StatusCode = 200,
+            Message = "Success",
+            Data = await _groupService.RetrieveAllAsync(@params)
+        });
+
+    /// <summary>
+    /// To Get group by id
+    /// </summary>
+    /// <param name="id"></param>
+    /// <returns></returns>
+    [HttpGet("{id}")]
+    public async Task<IActionResult> GetAsync([FromRoute(Name = "id")] long id)
+        => Ok(new Response
+        {
+            StatusCode = 200,
+            Message = "Success",
+            Data = await _groupService.RetrieveByIdAsync(id)
+        });
+
+    /// <summary>
+    /// To update group by id
+    /// </summary>
+    /// <param name="id"></param>
+    /// <param name="dto"></param>
+    /// <returns></returns>
+    [HttpPut("{id}")]
+    public async Task<IActionResult> PutAsync([FromRoute(Name = "id")] long id, [FromBody] GroupForUpdateDto dto)
+        => Ok(new Response
+        {
+            StatusCode = 200,
+            Message = "Success",
+            Data = await _groupService.ModifyAsync(id, dto)
+        });
+
+    /// <summary>
+    /// To delete by id
+    /// </summary>
+    /// <param name="id"></param>
+    /// <returns></returns>
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeleteAsync([FromRoute(Name = "id")] long id)
+        => Ok(new Response
+        {
+            StatusCode = 200,
+            Message = "Success",
+            Data = await _groupService.RemoveAsync(id)
+        });
+
+    /// <summary>
+    /// To get all groups by searching
+    /// </summary>
+    /// <param name="search"></param>
+    /// <param name="params"></param>
+    /// <returns></returns>
+    [HttpGet("search")]
+    public async Task<IActionResult> GetAllBySearchAsync([FromQuery(Name = "search")] string search, [FromQuery] PaginationParams @params)
+        => Ok(new Response
+        {
+            StatusCode = 200,
+            Message = "Success",
+            Data = await _groupService.SearchAllAsync(search, @params)
+        });
+}
